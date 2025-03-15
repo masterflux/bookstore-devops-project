54928395fde46e9d5aa0e63ed9e85007891d5826
Calvin <menglinlun@gmail.com>
2025-03-15 16:28:24 +0000

    Update route.ts

--- frontend/bookstorefe/src/app/api/books/route.ts
+++ frontend/bookstorefe/src/app/api/books/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(req: NextResponse ) {
+export async function POST(req: NextResponse) {
     const body = await req.json();
     return new Response(JSON.stringify({ message: `Hello, ${body.name}!` }), {
         status: 200,
@@ -9,15 +9,15 @@ export async function POST(req: NextResponse ) {
 }
 
 export async function GET(request: NextRequest) {
-    const url = new URL(request.url);
-    const category = url.searchParams.get('category');
-    let backendUrl = process.env.CATALOGURL || 'http://localhost:3001';
-    if (category) {
-        backendUrl += `/books/category/${category}`
-    } else {
-        backendUrl += '/books'
-    }
     try {
+        const category = request.url.split('?')[1]?.split('category=')[1];
+        let backendUrl = process.env.CATALOGURL || 'http://localhost:3001';
+        if (category) {
+            backendUrl += `/books/category/${category}`
+        } else {
+            backendUrl += '/books'
+        }
+
         const backendRes = await fetch(backendUrl)
         if (!backendRes.ok) {
             return new Response(JSON.stringify([]), {
@@ -27,9 +27,9 @@ export async function GET(request: NextRequest) {
         }
         const data = await backendRes.json()
         return new Response(JSON.stringify(data), {
-                status: 200,
-                headers: { 'Content-Type': 'application/json' },
-            })
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        })
     } catch (error) {
         console.log(error);
         return new Response(JSON.stringify([]), { status: 500 })
