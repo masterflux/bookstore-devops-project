1fc641e5a9e0e308ee285dcd8926048dcaad5225
AthulRajagopal07 <87569106+AthulRajagopal07@users.noreply.github.com>
2025-03-15 22:12:56 -1200

    Update index.js

--- backend/catalogservice/index.js
+++ backend/catalogservice/index.js
@@ -49,7 +49,7 @@ app.get('/books/category/:category', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:` + port);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:` + port);
 });
 
