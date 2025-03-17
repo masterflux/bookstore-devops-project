e00bee1262996b1d1fca71b605a15e9ec1ebad6b
Vibhav <vibmahajan@gmail.com>
2025-03-17 23:27:29 +0000

    removed comments and added readiness probes

--- loadtest.js
+++ loadtest.js
@@ -3,10 +3,8 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 1000 },
-        // Sustain 1000 VUs for 2 minutes
-        { duration: '2m', target: 1000 },
-        // Ramp down to 0 VUs in 1 minute
+        { duration: '1m', target: 500 },       
+        { duration: '2m', target: 500 },
         { duration: '1m', target: 0 },
     ],
   };
@@ -15,5 +13,5 @@ export default function () {
   let res = http.get('http://131.145.8.161');
  // let res = http.get('http://131.145.8.161/catalog/science?_rsc=zs6pb');
   check(res, { 'status is 200': (r) => r.status === 200 });
-  sleep(1); // wait for 1 second between iterations
+  sleep(1);
 }
