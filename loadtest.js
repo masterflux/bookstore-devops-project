d16aab9b0d428bfd37ca09f531bfc9d3e75b3a6e
Vibhav <vibmahajan@gmail.com>
2025-03-18 12:15:03 +0000

    Final implementation

--- loadtest.js
+++ loadtest.js
@@ -3,15 +3,14 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 1000 },       
-        { duration: '2m', target: 1000 },
+        { duration: '1m', target: 500 },       
+        { duration: '5m', target: 500 },
         { duration: '1m', target: 0 },
     ],
   };
   
 export default function () {
   let res = http.get('http://131.145.91.125');
- // let res = http.get('http://131.145.8.161/catalog/science?_rsc=zs6pb');
   check(res, { 'status is 200': (r) => r.status === 200 });
   sleep(1);
 }
